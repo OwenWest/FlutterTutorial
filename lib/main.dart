47e7676b3528812fc757df12f62d0a48b83b3c2d
OwenWest <64919158+OwenWest@users.noreply.github.com>
2020-09-13 14:40:49 -0700

    Testing this out

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+void main() => runApp(MaterialApp(
+  home: Home()
+));
+
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('hello world'),
+        centerTitle: true,
+        backgroundColor: Colors.blue[800],
+      ),
+      body: Center(
+        child: Text(
+            '2020 blows',
+            style: TextStyle(
+              fontSize: 20.0,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 1.5,
+              color: Colors.grey[600],
+            )
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Text(
+            "+",
+            style: TextStyle(
+                fontSize: 40.0)
+        ),
+        backgroundColor: Colors.blue[800],
+      ),
+    );
+  }
+}
+
+
+
+
+
